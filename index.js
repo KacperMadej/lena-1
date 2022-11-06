16db598fd05d9738d737d084adb3241c2f9d921f
Kacper Madej <kacper@blacklabel.pl>
2022-11-06 03:07:36 +0100

    Rounded colored border

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ function getColor() {
     return `rgb(${getRand(255)},${getRand(255)},${getRand(255)})`;
 }
 
+function opacities(color, opacity) {
+    const parts = color.split(/[()]/);
+    return `rgba(${parts[1]},${opacit})`;
+}
+
 root.onclick = () => {
     const newColor = getColor();
     root.style['background-color'] = newColor;
-    info.innerHTML = newColor;
+    root.style['border-color'] = opacities(newColor, 0.5) + ' black';
+    info.innerHTML = newColor + '_' + opacities(newColor, 0.5);
 };
